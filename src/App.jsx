9954492ffd84d5873b4456a61194bfc3967f56b3
mark-marshall <mark.marshallgp@gmail.com>
2019-02-15 14:48:20 +0000

    Added 9 square grid and basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from './components/Container';
+import './App.less';
 
 ReactDOM.render(<Container />, document.querySelector('#target1'));
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,13 +4,23 @@ class Container extends React.Component {
   constructor() {
     super();
     this.state = {
-      initial: 0,
-    }
+      initial: 0
+    };
   }
 
   render() {
     return (
-      <div className="container">Hello World</div>
+      <div className="container">
+        <div className="square" data-tab={1}>1</div>
+        <div className="square" data-tab={2}>2</div>
+        <div className="square" data-tab={3}>3</div>
+        <div className="square" data-tab={4}>4</div>
+        <div className="square" data-tab={5}>5</div>
+        <div className="square" data-tab={6}>6</div>
+        <div className="square" data-tab={7}>7</div>
+        <div className="square" data-tab={8}>8</div>
+        <div className="square" data-tab={9}>9</div>
+      </div>
     );
   }
 }
