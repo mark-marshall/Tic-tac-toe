49d042a66b889569e031d052dbb22217bfa076d8
mark-marshall <mark.marshallgp@gmail.com>
2019-02-15 15:01:38 +0000

    Added userFillSquare method and onClick event handler for each square

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -16,18 +16,25 @@ class Container extends React.Component {
     };
   }
 
+  userFillSquare = (square) => {
+    this.setState({
+      [square]: 'O',
+    })
+  }
+  
+
   render() {
     return (
       <div className="container">
-        <div className="square" data-tab={1}>{this.state.SquareOne}</div>
-        <div className="square" data-tab={2}>{this.state.SquareTwo}</div>
-        <div className="square" data-tab={3}>{this.state.SquareThree}</div>
-        <div className="square" data-tab={4}>{this.state.SquareFour}</div>
-        <div className="square" data-tab={5}>{this.state.SquareFive}</div>
-        <div className="square" data-tab={6}>{this.state.SquareSix}</div>
-        <div className="square" data-tab={7}>{this.state.SquareSeven}</div>
-        <div className="square" data-tab={8}>{this.state.SquareEight}</div>
-        <div className="square" data-tab={9}>{this.state.SquareNine}</div>
+        <button onClick={() => this.userFillSquare('SquareOne')} className="square" data-tab={1}>{this.state.SquareOne}</button>
+        <button onClick={() => this.userFillSquare('SquareTwo')} className="square" data-tab={2}>{this.state.SquareTwo}</button>
+        <button onClick={() => this.userFillSquare('SquareThree')} className="square" data-tab={3}>{this.state.SquareThree}</button>
+        <button onClick={() => this.userFillSquare('SquareFour')} className="square" data-tab={4}>{this.state.SquareFour}</button>
+        <button onClick={() => this.userFillSquare('SquareFive')} className="square" data-tab={5}>{this.state.SquareFive}</button>
+        <button onClick={() => this.userFillSquare('SquareSix')} className="square" data-tab={6}>{this.state.SquareSix}</button>
+        <button onClick={() => this.userFillSquare('SquareSeven')} className="square" data-tab={7}>{this.state.SquareSeven}</button>
+        <button onClick={() => this.userFillSquare('SquareEight')} className="square" data-tab={8}>{this.state.SquareEight}</button>
+        <button onClick={() => this.userFillSquare('SquareNine')} className="square" data-tab={9}>{this.state.SquareNine}</button>
       </div>
     );
   }
